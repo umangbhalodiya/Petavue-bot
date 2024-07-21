a749346af71aa9550835e8299645db830763a36a
umangbhalodiya <umangbhalodiya660@gmail.com>
2024-07-21 12:35:15 +0530

    Bot script added, styles changes, bot component styles modifications.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Bot from "./components/Bot";
 import styles from "./styles/common.module.scss";
 
 function App() {
   return (
     <div className={styles.appSection}>
-      <div className={styles.botIcon}>Bot</div>
+      <div className={styles.botIcon}>{/* <Bot /> */}</div>
     </div>
   );
 }
--- src/components/Bot/index.jsx
+++ src/components/Bot/index.jsx
@@ -2,7 +2,7 @@ import "./bot.module.scss";
 export default function Bot() {
   return (
     <div>
-      <div></div>
+      <div>bott</div>
     </div>
   );
 }
